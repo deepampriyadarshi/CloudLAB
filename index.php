1979f97687335ea84f9775db1bd74c94ca8d2200
Deepam Priyadarshi <65447610+deepampriyadarshi@users.noreply.github.com>
2022-04-25 12:58:06 +0530

    adding index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>User Profile</title>
+	</head>
+	<body>
+		<form action="adduser.php" method="post">
+			Enter Your Name: <input type="text" name="name"><br/>
+			Enter Your Age: <input type="number" name="age"><br/>
+			Enter Your Registration Number: <input type="text" name="name"><br/>
+			<input type="submit" value="Submit Data"><br/>
+		</form>
+	</body>
+</head>
